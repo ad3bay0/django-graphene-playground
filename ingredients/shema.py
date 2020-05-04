35bdac9e85528f0c37966270add44012aec93fe6
ad3bay0 <byoextreme@hotmail.com>
2020-05-04 02:08:36 +0100

    chore: query single object

--- ingredients/shema.py
+++ ingredients/shema.py
@@ -4,11 +4,40 @@ from .types import CategoryType, IngredientType
 from .models import Category, Ingredient
 
 class Query(object):
+    category = graphene.Field(CategoryType,
+                              id=graphene.Int(),
+                              name=graphene.String())
     all_categories = graphene.List(CategoryType)
+    
+    ingredient = graphene.Field(
+        IngredientType,
+        id=graphene.Int(),
+        name=graphene.String()
+    )
     all_ingredients = graphene.List(IngredientType)
+    
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+        if id is not None:
+            return Category.objects.get(pk=id)
+        if name is not None:
+            return Category.objects.get(name=name)
+        
+        return None
     
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all();
+    
+    def resolve_ingredient(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+        if id is not None:
+            return Ingredient.objects.get(pk=id)
+        if name is not None:
+            return Ingredient.objects.get(name=name)
+        
+        return None
     
     def resolve_all_ingredients(self, info, **kwargs):
         return Ingredient.objects.all();
--- ingredients/types.py
+++ ingredients/types.py
@@ -2,11 +2,18 @@ import graphene
 
 from graphene_django.types import DjangoObjectType
 from .models import Category, Ingredient
+from graphene_django_extras.paginations import LimitOffsetGraphqlPagination
 
 class CategoryType(DjangoObjectType):
     class Meta:
         model = Category
+        # ordering can be: string, tuple or list
+        pagination = LimitOffsetGraphqlPagination(default_limit=2, ordering="-name")
         
 class IngredientType(DjangoObjectType):
     class Meta:
         model = Ingredient
+         # ordering can be: string, tuple or list
+        pagination = LimitOffsetGraphqlPagination(
+            default_limit=2, ordering="-name")
+        
